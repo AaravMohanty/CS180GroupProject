ab1fe101ae19135d8028a3051880ed2445fa1f87
knitzsc <knitzsc@purdue.edu>
2024-11-03 16:48:18 -0600

    I fixed the interfaces

--- theDatabase/src/MessageInterface.java
+++ theDatabase/src/MessageInterface.java
@@ -1,18 +1,19 @@
-//import java.io.File;
-//
-//public interface MessageInterface {
-//        // Gets the receiver of the message
-//    String getReceiver();
-//
-//    // Gets the content of the message
-//    String getContent();
-//
-//    // Gets the photo associated with the message
-//    File getPhoto();
-//
-//    // Checks if the message is a photo message
-//    boolean isPhotoMessage();
-//
-//    //writes the message to a file
-//    void writeMessageToFile(String conversationFileName);
-//}
+import java.io.File;
+
+public interface MessageInterface {
+
+    // Gets the username of the message receiver
+    String getReceiver();
+
+    // Gets the text content of the message, or null if it is a photo message
+    String getContent();
+
+    // Gets the photo file associated with the message, or null if it is a text message
+    File getPhoto();
+
+    // Checks if the message is a photo message (returns true if the message contains a photo, false otherwise)
+    boolean isPhotoMessage();
+
+    // Writes the message to the specified conversation file
+    void writeMessageToFile(String conversationFileName);
+}
