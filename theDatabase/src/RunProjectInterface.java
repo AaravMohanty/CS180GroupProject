dc4b2b2315668fd060a4d9e2a6080c0a2a3e99c2
elansmyla <esmyla@purdue.edu>
2024-11-03 20:58:42 -0500

    finished

--- theDatabase/src/RunProjectInterface.java
+++ theDatabase/src/RunProjectInterface.java
@@ -1,30 +1,11 @@
-import java.util.*;
-
 public interface RunProjectInterface {
-    // Method to create a new user account
-    void createAccount();
-
-    // Method for user login, returns a User if successful
-    User login();
-
-    // Method to add a friend
-    void addFriend();
-
-    // Method to remove a friend
-    void removeFriend();
-
-    // Method to block or unblock a user
-    void blockUser();
-
-    // Method to send a message
-    void sendMessage();
-
-    // Method to view a user profile
-    void viewUserProfile();
-
-    // Method to display available users and view a selected profile
-    void search();
-
-    // Method to view a specific user's profile
-    void viewUserProfile(User user);
+    void createAccount();      // Method for creating a new user account
+    User login();              // Method for logging in an existing user
+    void addFriend();          // Method to add a friend
+    void removeFriend();       // Method to remove a friend
+    void blockUser();          // Method to block or unblock a user
+    void sendMessage();        // Method to send a message to another user
+    void viewUserProfile();    // Method to view a user's profile by username
+    void search();             // Method to search for users and view profiles
+    void viewUserProfile(User user); // Method to view a specific user's profile
 }
