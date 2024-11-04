8980f6cbd030b8d1adf65817a5fd5b4cce07b360
esmyla <esmyla@purdue.edu>
2024-11-04 00:32:31 -0500

    Update MessageInterfaceTest.java

--- theDatabase/src/MessageInterfaceTest.java
+++ theDatabase/src/MessageInterfaceTest.java
@@ -3,9 +3,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.File;
 
 /**
- * The message interface junit class to test message interface
+ * The message test to store message information
  *
- * @version November 3, 2024
+ * Purdue University -- CS18000 -- Fall 2024 
+ *
+ * @author Elan Smyla, 11
+ * @version November 3rd, 2024
  */
 
 class MessageInterfaceTest {
