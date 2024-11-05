4956f9e63de206d0854a1f906699af6da31abac6
esmyla <esmyla@purdue.edu>
2024-11-04 20:01:34 -0500

    Update Message.java

--- theDatabase/src/Message.java
+++ theDatabase/src/Message.java
@@ -2,8 +2,8 @@ import java.io.*;
 
 /**
  * The message class to store message information
- *
- * Purdue University -- CS18000 -- Fall 2024 
+ * <p>
+ * Purdue University -- CS18000 -- Fall 2024
  *
  * @author Elan Smyla, Aarav Mohanty
  * @version November 3rd, 2024
