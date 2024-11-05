cf198930bfa73b87d3db8c8347fe997c1bc821ab
esmyla <esmyla@purdue.edu>
2024-11-04 20:00:57 -0500

    Update DatabaseInterface.java

--- theDatabase/src/DatabaseInterface.java
+++ theDatabase/src/DatabaseInterface.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 /**
  * A program that creates the database interface.
- *
- * Purdue University -- CS18000 -- Fall 2024 
+ * <p>
+ * Purdue University -- CS18000 -- Fall 2024
  *
  * @author Elan Smyla, Aarav Mohanty
  * @version November 3rd, 2024
