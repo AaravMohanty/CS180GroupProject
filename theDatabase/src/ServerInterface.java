196fe41836052ec477a49596ecccbfa940b6b41d
esmyla <esmyla@purdue.edu>
2024-11-17 17:28:55 -0500

    Update ServerInterface.java

--- theDatabase/src/ServerInterface.java
+++ theDatabase/src/ServerInterface.java
@@ -1,2 +1,11 @@
+/**
+ * The ServerInterface interface is implemented by the Server class.
+ * <p>
+ * Purdue University -- CS18000 -- Fall 2024
+ *
+ * @author Elan Smyla, Aarav Mohanty, Hannah Cha, Kai Nietzche
+ * @version November 17th, 2024
+ */
+
 public interface ServerInterface {
 }
